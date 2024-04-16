4f6190efcbea41c19272539b7f5f4e93fabc4035
jiadongwei <602914623@qq.com>
2024-04-16 13:19:20 +0800

    sql拦截器配置。

---
+++ jc-club-subject/jc-club-infra/src/main/java/com/jia/subject/infra/config/MybatisConfiguration.java
@@ -0,0 +1,18 @@
+package com.jia.subject.infra.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class MybatisConfiguration {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new MybatisPlusAllSqlLog());
+        return mybatisPlusInterceptor;
+    }
+
+}
