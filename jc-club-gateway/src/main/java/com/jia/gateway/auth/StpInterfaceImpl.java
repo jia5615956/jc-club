ed06de92cd38f7b37d4ca3499db3a99042fa7367
jiadongwei <602914623@qq.com>
2024-04-30 16:14:15 +0800

    redis工具类

--- jc-club-gateway/src/main/java/com/jia/gateway/auth/StpInterfaceImpl.java
+++ jc-club-gateway/src/main/java/com/jia/gateway/auth/StpInterfaceImpl.java
@@ -1,8 +1,10 @@
 package com.jia.gateway.auth;
 
 import cn.dev33.satoken.stp.StpInterface;
+import com.jia.gateway.redis.RedisUtil;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
@@ -11,9 +13,16 @@ import java.util.List;
 @Component
 public class StpInterfaceImpl implements StpInterface {
 
+    @Resource
+    private RedisUtil redisUtil;
+
+    private String authPrefix = "auth.permission";
+    private String authRolePrefix = "auth.role";
+
     @Override
     public List<String> getPermissionList(Object loginId, String loginType) {
         // 返回此 loginId 拥有的权限列表
+
         return null;
     }
 
