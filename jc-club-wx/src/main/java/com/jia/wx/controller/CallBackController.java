5c4695d4f33ecbd55baf2d85d071b3ddb0ab86e9
jiadongwei <602914623@qq.com>
2024-05-08 19:58:13 +0800

    wx校验

--- jc-club-wx/src/main/java/com/jia/wx/controller/CallBackController.java
+++ jc-club-wx/src/main/java/com/jia/wx/controller/CallBackController.java
@@ -1,14 +1,49 @@
 package com.jia.wx.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.jia.wx.utils.SHA1;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
+@Slf4j
 public class CallBackController {
 
+
+    private static final String token = "adwidhaidwoaid";
+
     @RequestMapping("/test")
-    public String test(){
+    public String test() {
         return "hello word";
+    }
+    /**
+     * 回调消息校验
+     */
+    @GetMapping("/callback")
+    public String callback(@RequestParam("signature") String signature,
+                           @RequestParam("timestamp") String timestamp,
+                           @RequestParam("nonce") String nonce,
+                           @RequestParam("echostr") String echostr) {
+        log.info("get验签请求参数：signature:{}，timestamp:{}，nonce:{}，echostr:{}",
+                signature, timestamp, nonce, echostr);
+        String shaStr = SHA1.getSHA1(token, timestamp, nonce, "");
+        if (signature.equals(shaStr)) {
+            return echostr;
+        }
+        return "unknown";
+    }
+
+
+    @PostMapping(value = "/callback", produces = "application/xml;charset=UTF-8")
+    public String callback(
+            @RequestBody String requestBody,
+            @RequestParam("signature") String signature,
+            @RequestParam("timestamp") String timestamp,
+            @RequestParam("nonce") String nonce,
+            @RequestParam(value = "msg_signature", required = false) String msgSignature) {
+        log.info("接收到微信消息：requestBody：{}", requestBody);
+
+        return "unknown";
     }
 }
